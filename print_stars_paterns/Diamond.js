69cfdb582236042e086fb6187df842de6bb56f4c
sama.khani <bt.sama.7art@gmail.com>
2024-02-01 00:43:42 +0330

    Diamond

---
+++ print_stars_paterns/Diamond.js
@@ -0,0 +1,25 @@
+const Diamond = (n) => {
+    let string = "";
+    for (let i = 1; i <= n; i++) {
+        for (let j = n; j > i; j--) {
+            string += " ";
+        }
+        for (let k = 0; k < i * 2 - 1; k++) {
+            string += "*";
+        }
+        string += "\n";
+    }
+    for (let i = 1; i <= n - 1; i++) {
+        for (let j = 0; j < i; j++) {
+            string += " ";
+        }
+        for (let k = (n - i) * 2 - 1; k > 0; k--) {
+            string += "*";
+        }
+        string += "\n";
+    }
+    return string;
+};
+
+const number = 5;
+console.log(Diamond(number));
